c2c4c0846ee6615beaac2df341b94227bf33efbe
Terry Chia <terry@ayrx.me>
2022-07-30 14:24:57 +0800

    Add vulnerable code

---
+++ foo.js
@@ -0,0 +1,33 @@
+// ok: eval-with-expression
+eval('alert')
+
+// ruleid: eval-with-expression
+eval(user_input)
+
+// ok: eval-with-expression
+window.eval('alert')
+
+// ruleid: eval-with-expression
+window.eval(user_input)
+
+// ok: eval-with-expression
+var x = Function('a', 'b', 'return 1')
+
+// ruleid: eval-with-expression
+var x = Function('a', 'b', user_input)
+
+// ok: eval-with-expression
+var x = new Function('a', 'b', 'return 1')
+
+// ruleid: eval-with-expression
+var x = new Function('a', 'b', user_input)
+
+// ruleid: eval-with-expression
+eval('alert(' + c + ')')
+
+// ruleid: eval-with-expression
+eval(`return ${funcName}(a,b)`)
+
+var f = "return 1"
+// ok: eval-with-expression
+var x = new Function('a', 'b', f)
